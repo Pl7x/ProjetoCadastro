b1689541e5a3b301c81a2713603fe0609c22cd64
Pedro Lucas <pedrocabral1707@gmail.com>
2026-05-15 02:05:28 -0300

    ajuste da config

--- app/config.php
+++ app/config.php
@@ -3,7 +3,7 @@ return [
     'database' => [
         'host' => 'localhost',
         'user' => 'root',
-        'password' => '',
+        'password' => 'Pedrolucas05.',
         'name' => 'escola',
     ],
 ];
